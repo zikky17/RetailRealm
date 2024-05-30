9b797ffcf9c25ceebcc2131497959563407ae583
Gustav Ivering <gustav.ivering@live.se>
2024-05-30 14:57:38 +0200

    Added connection string

---
+++ RetailRealm/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RetailRealm.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
