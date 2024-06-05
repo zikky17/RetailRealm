6d8016618bc1a56aff7c6bca7c8f00b04ae140b1
Gustav Ivering <gustav.ivering@live.se>
2024-06-05 15:31:44 +0200

    Added Company dropdown that only shows if user is a Company user

--- DataAccessLibrary/Migrations/20240605131146_Added comapny to User.Designer.cs
+++ DataAccessLibrary/Migrations/20240605131146_Added comapny to User.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RetailRealm.DataAccessLibrary.Data;
 
@@ -11,9 +12,11 @@ using RetailRealm.DataAccessLibrary.Data;
 namespace DataAccessLibrary.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240605131146_Added comapny to User")]
+    partial class AddedcomapnytoUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
