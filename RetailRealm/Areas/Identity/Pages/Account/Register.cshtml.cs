4fa84af2027f26f41ae0ca11ab9b405a70aa48d8
Gustav Ivering <gustav.ivering@live.se>
2024-06-05 06:55:46 +0200

    Added more properties to Register page

--- RetailRealm/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ RetailRealm/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -109,6 +109,13 @@ namespace RetailRealm.Areas.Identity.Pages.Account
             public string? Role {  get; set; }
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
         }
 
 
