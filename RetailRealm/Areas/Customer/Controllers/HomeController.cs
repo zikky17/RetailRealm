89f00f98d017d82601fc8eb472d6b7519b15a8b7
Gustav Ivering <gustav.ivering@live.se>
2024-06-06 07:59:25 +0200

    Added shopping cart UI and controller

--- RetailRealm/Areas/Customer/Controllers/HomeController.cs
+++ RetailRealm/Areas/Customer/Controllers/HomeController.cs
@@ -57,6 +57,7 @@ namespace RetailRealm.Areas.Customer.Controllers
             {
                 _unitOfWork.ShoppingCartRepository.Add(cart);
             }
+            TempData["success"] = "Cart updated successfully";
 
             _unitOfWork.Save();
 
---
+++ RetailRealm/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RetailRealm.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ShoppingCartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
