5306571d0e42213a0564698bbc40035b136f01b1
Gustav Ivering <gustav.ivering@live.se>
2024-06-10 06:53:07 +0200

    Shipping logic is in place for different Roles.

--- RetailRealm/Areas/Admin/Controllers/OrderController.cs
+++ RetailRealm/Areas/Admin/Controllers/OrderController.cs
@@ -4,11 +4,13 @@ using Microsoft.AspNetCore.Mvc;
 using ModelsLibrary.Models;
 using ModelsLibrary.ViewModels;
 using System.Diagnostics;
+using System.Security.Claims;
 using UtilitiesLibrary;
 
 namespace RetailRealm.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize] 
     public class OrderController : Controller
     {
 
@@ -64,6 +66,39 @@ namespace RetailRealm.Areas.Admin.Controllers
             return RedirectToAction(nameof(Details), new { orderId = orderHeaderFromDb.OrderId });
         }
 
+        [HttpPost]
+        [Authorize(Roles = StaticDetails.Role_Admin + "," + StaticDetails.Role_Employee)]
+        public IActionResult StartProcessing()
+        {
+            _unitOfWork.OrderHeaderRepository.UpdateStatus(OrderVM.OrderHeader.OrderId, StaticDetails.StatusInProcess);
+            _unitOfWork.Save();
+            TempData["success"] = "Order Details Updated Successfully.";
+            return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.OrderId });
+
+        }
+
+        [HttpPost]
+        [Authorize(Roles = StaticDetails.Role_Admin + "," + StaticDetails.Role_Employee)]
+        public IActionResult ShipOrder()
+        {
+
+            var orderHeader = _unitOfWork.OrderHeaderRepository.GetOne(u => u.OrderId == OrderVM.OrderHeader.OrderId);
+            orderHeader.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+            orderHeader.Carrier = OrderVM.OrderHeader.Carrier;
+            orderHeader.OrderStatus = StaticDetails.StatusShipped;
+            orderHeader.ShippingDate = DateTime.Now;
+            if(orderHeader.PaymentStatus == StaticDetails.PaymentStatusDelayedPayment)
+            {
+                orderHeader.PaymentDueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(30));
+            }
+
+            _unitOfWork.OrderHeaderRepository.Update(orderHeader);
+            _unitOfWork.Save();
+            TempData["success"] = "Order Shipped Successfully.";
+            return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.OrderId });
+
+        }
+
 
         #region API CALLS
 
@@ -71,7 +106,20 @@ namespace RetailRealm.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll(string status)
         {
-            IEnumerable<OrderHeader> orders = _unitOfWork.OrderHeaderRepository.GetAll(null, "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> orders;
+
+            if(User.IsInRole(StaticDetails.Role_Admin) || User.IsInRole(StaticDetails.Role_Employee))
+            {
+                orders = _unitOfWork.OrderHeaderRepository.GetAll(null, "ApplicationUser").ToList();
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                orders = _unitOfWork.OrderHeaderRepository
+                    .GetAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser");
+            }
 
             switch (status)
             {
